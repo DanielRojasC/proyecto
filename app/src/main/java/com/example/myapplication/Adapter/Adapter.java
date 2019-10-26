96e2d0fa59aff30323110cc788ed0280c83f64a1
fibrousfall <leansanchez14@gmail.com>
2019-10-26 14:48:46 -0500

    adapter

---
+++ app/src/main/java/com/example/myapplication/Adapter/Adapter.java
@@ -0,0 +1,46 @@
+package com.example.myapplication.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.myapplication.R;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    private String[] mDataset;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView textView;
+        public MyViewHolder(TextView v) {
+            super(v);
+            textView = v;
+        }
+    }
+
+    public Adapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+
+    @Override
+    public Adapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                      int viewType) {
+
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_list, parent, false);
+                MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.textView.setText(mDataset[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
