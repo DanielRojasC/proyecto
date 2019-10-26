bb77f22493d4664d5003911aa882a5d6e0f1db46
fibrousfall <leansanchez14@gmail.com>
2019-10-26 17:25:20 -0500

    creo que hice algo

--- app/src/main/java/com/example/myapplication/Adapter/Adapter.java
+++ app/src/main/java/com/example/myapplication/Adapter/Adapter.java
@@ -1,46 +1,79 @@
 package com.example.myapplication.Adapter;
 
-import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.RecyclerView;
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+//import androidx.recyclerview.widget.RecyclerView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
-    private String[] mDataset;
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
-        public MyViewHolder(TextView v) {
-            super(v);
-            textView = v;
+        private Context mcontext;
+        private List<Usuario> mUsers;
+
+
+        public Adapter(Context mContext, List<Usuario> mUsers){
+        this.mUsers = mUsers;
+        this.mcontext = mContext;
         }
-    }
 
-    public Adapter(String[] myDataset) {
-        mDataset = myDataset;
-    }
+        @NonNull
+        @Override
+        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
+        View view = LayoutInflater.from(mcontext).inflate(R.layout.item_list,parent,false);
+        return new Adapter.ViewHolder(view);
+        }
 
-    @Override
-    public Adapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                      int viewType) {
+        @Override
+        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_list, parent, false);
-                MyViewHolder vh = new MyViewHolder(v);
-        return vh;
-    }
+        /*final User user = mUsers.get(position);
+                holder.username.setText(user.getUsername());
+                if (user.getImgUrl() == null){
+                holder.profile_img.setImageResource(R.mipmap.ic_launcher);
+                }else {
+                Glide.with(mcontext).load(user.getImgUrl()).into(holder.profile_img);
+                }
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        Intent intent = new Intent(mcontext, MessageActivity.class);
+        intent.putExtra("userid",user.getId());
+        mcontext.startActivity(intent);
+        }
+        });*/
 
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(mDataset[position]);
-
-    }
+        }
 
     @Override
     public int getItemCount() {
-        return mDataset.length;
+            return mUsers.size();
+            }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+    public TextView username;
+    public ImageView profile_img;
+
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        username = itemView.findViewById(R.id.username);
+        profile_img = itemView.findViewById(R.id.profile_img);
+
+
+
     }
+}
 }
